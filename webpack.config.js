64012d683af256b45e2566f2003648d675812566
Gagan M <theTechie@users.noreply.github.com>
2016-08-26 09:58:35 +0530

    update packages; upgrade react-hot-loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = function(component_to_render, sourcePath) {
       module: {
         loaders: [{
           test: /\.js?$/,
-          loaders: ['react-hot', 'babel'],
+          loader: 'babel',
           include: [path.join(__dirname, 'src'), sourcePath],
           exclude: /node_modules/,
       },
